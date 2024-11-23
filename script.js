277e1859df88ce006e124f4a4326a84e711056cb
vishal rajpoot <noobcoderr440@gmail.com>
2024-11-23 13:26:41 +0530

    add inital files

---
+++ script.js
@@ -0,0 +1,37 @@
+// function call (){
+//     var value = 10;
+//     value=12;
+//     console.log(value);
+//     {
+//         let value1 = 20; 
+//          value1 = 23; 
+//         console.log(value1);
+//         console.log(value);
+//     }
+//     console.log(value1);
+// }
+// call();
+// // console.log(value);
+
+// var x = 5;
+// var y = ++x; 
+
+// // post
+
+// let x = 5;
+// let y = x++;  
+
+// var typed = new typed('.typer', {
+//     strings: ("web Developer", "Software Developer", "youtuber"),
+//     typeSpeed: 70,
+//     backSpeed: 60,
+//     loop: true
+// });
+let btn = document.getElementById(btn);
+btn.addEventListener('click',function (event)
+
+[
+    event.stopImmediatePropagation();
+])
+
+ 
